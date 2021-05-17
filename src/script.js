974c4a5c912f832ab93646f82e4eba6a3a02756a
Mafalda <84099790+a-Mafalda@users.noreply.github.com>
2021-05-17 17:47:59 +0100

    updated time JS

--- src/script.js
+++ src/script.js
@@ -1,3 +1,23 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+      hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+      minutes = `0${minutes}`;
+  }
+  
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  let month = months[date.getMonth()];
+  let todayDate = date.getDate();
+  return `${day}, ${month} ${todayDate} ${hours}:${minutes}` ;
+}
+
+
 function showTemperature(response){
 console.log(response.data);
 let temperature = document.querySelector("#temperature");
@@ -16,8 +36,8 @@ let humidity = document.querySelector("#humidity");
 humidity.innerHTML= Math.round(response.data.main.humidity);
 let wind = document.querySelector("#wind");
 wind.innerHTML= Math.round(response.data.wind.speed);
-
-
+let date = document.querySelector("#date");
+date.innerHTML= formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "656ac87c5034b9f4933b4a4211cbca36";
